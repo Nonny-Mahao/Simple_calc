d8305d3712facbc2424deb059610ac94f50ff797
Nonny177 <55391624+Nonny177@users.noreply.github.com>
2021-06-15 14:20:22 +0200

    Added the subtraction and division functions

--- index.js
+++ index.js
@@ -3,5 +3,17 @@ function add(num1,num2) {
 	let sum = num1 + num2;
 	console.log(sum);	
 }
+function subtract(x, y){
+    let difference = x - y;
+    console.log(difference);
+}
+
+function divide(n, m){
+    let quotient = n/m;
+    console.log(quotient);
+}
+
 
 add(2,4);
+subtract(2,4);
+divide(2,4);
